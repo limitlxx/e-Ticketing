ce42ef70e3e306f6e7596c36ebf8c02b8c3be88f
limitlxx <elgravicodesh@gmail.com>
2024-09-05 13:57:20 +0100

    readMe: error

--- e-ticket/src/app/components/eventCard.js
+++ e-ticket/src/app/components/eventCard.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { abi } from "../../../utils/abi";
 import { contractAddress } from "../../../utils/services";
-import { Notifications } from "./notification"
+import { Notifications } from "./notification";
 
 import { useAccount, useReadContract } from "wagmi";
 import { useRouter } from "next/navigation"; 
@@ -12,7 +12,7 @@ import { useEffect } from "react";
 export default function EventCard() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
-  const [error, setError] = useState(null); // Set to false initially, so the drawer is closed by default
+  const [error, setError] = useState(null);
    
   const { data: contractData, isError } = useReadContract({
     address: contractAddress,
@@ -33,72 +33,72 @@ export default function EventCard() {
   }, [contractData, isError]);
 
   if (loading) {
-    <Notifications />
+     <Notifications />;
   }
 
   if (error) {
     return <div>{error}</div>;
   }
 
-  console.log(contractAddress);
-  
+  if (!data || data.length === 0) {
+    return <div>No events found.</div>;
+  }
 
   return (
-    
     <div className="container mx-auto p-4">
       <h1 className=" ml-10 text-2xl font-semibold">Events</h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 my-8 sm:mx-10 gap-6">
-      {contractData.map((event) => (
-        <Link href={`/event/${event.id}`} key={event.id}>
-           <div className="bg-white rounded-xl shadow-custom-shadow hover:shadow-lg transition-shadow duration-300">
-            <div className="md:flex">
-              <div className="md:shrink-0">
-                <img
-                  className="h-48 w-full object-cover md:h-full xl:w-[20rem]"
-                  src="/Frame 13.svg"
-                  alt="Event Image"
-                />
-              </div>
-              <div className="p-6">
-                <div className="uppercase tracking-wide text-sm text-gray-500 font-semibold">
-                  7th September 2024
+        {data.map((event) => (
+          <Link href={`/event/${event.id}`} key={event.id}>
+            <div className="bg-white rounded-xl shadow-custom-shadow hover:shadow-lg transition-shadow duration-300">
+              <div className="md:flex">
+                <div className="md:shrink-0">
+                  <img
+                    className="h-48 w-full object-cover md:h-full xl:w-[20rem]"
+                    src="/Frame 13.svg"
+                    alt="Event Image"
+                  />
                 </div>
-                <h2 className="block mt-1 text-xl leading-tight font-bold text-[#364F6B]">
-                  How Using VAs & AI Can Help You Boost Productivity And Close
-                  More Deals
-                </h2>
-                <div className="flex text-sm my-3 items-center justify-between">
-                  <p className="flex items-center text-[#765BFF] font-semibold">
-                    <img
-                      className="h-[15px] mx-1"
-                      src="/placeholder.svg"
-                      alt="Location"
-                    />
-                    Lagos, Nigeria
-                  </p>
-                  <p className="font-medium text-base text-[#364F6B]">
-                    10:00AM
-                  </p>
-                </div>
-                <div className="flex items-center justify-between mt-4">
-                  <p className="flex items-center text-sm">
-                    <img src="/Group.svg" alt="Attendees" className="mr-2" />{" "}
-                    10+ Users attending
-                  </p>
-                  <button className="bg-gradient-to-r from-slate-200 to-slate-400 px-4 py-1 text-sm rounded-xl">
-                    Opened
-                  </button>
+                <div className="p-6">
+                  <div className="uppercase tracking-wide text-sm text-gray-500 font-semibold">
+                    7th September 2024
+                  </div>
+                  <h2 className="block mt-1 text-xl leading-tight font-bold text-[#364F6B]">
+                    How Using VAs & AI Can Help You Boost Productivity And Close
+                    More Deals
+                  </h2>
+                  <div className="flex text-sm my-3 items-center justify-between">
+                    <p className="flex items-center text-[#765BFF] font-semibold">
+                      <img
+                        className="h-[15px] mx-1"
+                        src="/placeholder.svg"
+                        alt="Location"
+                      />
+                      Lagos, Nigeria
+                    </p>
+                    <p className="font-medium text-base text-[#364F6B]">
+                      10:00AM
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-between mt-4">
+                    <p className="flex items-center text-sm">
+                      <img src="/Group.svg" alt="Attendees" className="mr-2" />{" "}
+                      10+ Users attending
+                    </p>
+                    <button className="bg-gradient-to-r from-slate-200 to-slate-400 px-4 py-1 text-sm rounded-xl">
+                      Opened
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
-     
-);
+  );
 }
+
 
 // import React from 'react';
 // import Link from 'next/link';
